f236c8f7a21e71c062637dfb871f9f9e5ff418ac
Stratos Staboulis <stratos.staboulis@gmail.com>
2022-01-25 08:54:04 +0200

    Add progress bar

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+
+char *progress_bar(char *bar, double rate) {
+  bar[0] = '[';
+  for (int i = 1; i <= 10; i++) {
+    if ((double) i / 10.0 <= rate) {
+      bar[i] = '=';
+    } else {
+      bar[i] = ' ';
+    }
+  }
+  bar[11] = ']';
+  return bar;
+}
+
+
 int main() {
+
+  char bar[12];
 
   // Image
   const int image_width = 256;
@@ -10,6 +27,10 @@ int main() {
   printf("P3\n%d %d\n255\n", image_width, image_height);
   for (int j = image_height-1; j >= 0; --j) {
     for (int i = 0; i < image_width; ++i) {
+
+      fprintf(stderr, "\r%s", progress_bar(bar, 1.0 - (double) j / image_height));
+      fflush(stdout);
+
       double r = (double) i / (image_width - 1);
       double g = (double) j / (image_height - 1);
       double b = 0.25;
