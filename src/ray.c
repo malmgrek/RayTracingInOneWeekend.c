f51516a5f57cb2949fd38decf4e96b3a03837cb2
Stratos Staboulis <stratos.staboulis@gmail.com>
2022-02-09 00:19:22 +0200

    Refactor ray.h with macros

--- src/ray.c
+++
@@ -1,9 +0,0 @@
-#include "ray.h"
-
-vec3_t ray_at(ray_t *ray, double t) {
-  vec3_t u;
-  u.x = ray->origin.x + t * ray->direction.x;
-  u.y = ray->origin.y + t * ray->direction.y;
-  u.z = ray->origin.z + t * ray->direction.z;
-  return u;
-}
--- src/ray.h
+++ src/ray.h
@@ -8,6 +8,9 @@ typedef struct {
   vec3_t direction;
 } ray_t;
 
-vec3_t ray_at(ray_t *ray, double t);
+static inline vec3_t ray_at(const ray_t *ray, double t) {
+  vec3_t t_dir = mul(t, &ray->direction);
+  return add(&ray->origin, &t_dir);
+}
 
 #endif // RAY_H_
